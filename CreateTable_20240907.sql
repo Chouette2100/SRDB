51bb0d82efc212007e4e3205e27c31dcb884c8b8
Chouette2100 <chouette.21.00@gmail.com>
2024-09-13 14:54:30 +0900

    20240913 CreateTable_20240907.sql DROPの追加と記号の誤りを訂正する

--- CreateTable_20240907.sql
+++ CreateTable_20240907.sql
@@ -1,34 +1,40 @@
+DROP TABLE IF EXISTS `giftscore`;
 CREATE TABLE `giftscore` (
   `giftid` int NOT NULL,
   `userno` int NOT NULL,
   `orderno` int NOT NULL,
   `score` int NOT NULL,
-  `status' varchar(100) NOT NULL,  #  追加 2024-09-07
+  `status` varchar(100) NOT NULL,  #  追加 2024-09-07
   `ts` datetime NOT NULL,
   PRIMARY KEY (`giftid`,`ts`,`userno`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+DROP TABLE IF EXISTS `viewergiftscore`;
 CREATE TABLE `viewergiftscore` (
   `giftid` int NOT NULL,
   `viewerid` int NOT NULL,
   `orderno` int NOT NULL,
   `score` int NOT NULL,
-  `status' varchar(100) NOT NULL,  #  追加 2024-09-08
+  `status` varchar(100) NOT NULL,  #  追加 2024-09-08
   `ts` datetime NOT NULL,
   PRIMARY KEY (`giftid`,`ts`,`viewerid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
+DROP TABLE IF EXISTS `viewer`;
 CREATE TABLE `viewer` (
   `viewerid` int NOT NULL,
   `name` varchar(512) NOT NULL,
+  `sname` varchar(512) NOT NULL,  #  2024-09-11
   `ts` datetime NOT NULL,
   PRIMARY KEY (`viewerid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+DROP TABLE IF EXISTS `viewerhistory`;
 CREATE TABLE `viewerhistory` (
   `viewerid` int NOT NULL,
   `name` varchar(512) NOT NULL,
+  `sname` varchar(512) NOT NULL,  #  2024-09-11
   `ts` datetime NOT NULL,
   PRIMARY KEY (`viewerid`, `ts`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
