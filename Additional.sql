11b9cfaf30911c962daf7b9df2bee8cf5e8d7ecf
Chouette2100 <chouette.21.00@gmail.com>
2024-10-03 08:56:31 +0900

    テーブルgiftscorecntrbをあらたに作成する

--- Additional.sql
+++ Additional.sql
@@ -40,3 +40,11 @@ UNLOCK TABLES;
 ALTER TABLE `showroom`.`giftranking` 
 ADD COLUMN `cntrblst` INT NOT NULL DEFAULT 0 AFTER `norder`;
 
+CREATE TABLE `showroom`.`giftscorecntrb` (
+  `giftid` INT NOT NULL,
+  `userno` INT NOT NULL,
+  `viewerid` INT NOT NULL,
+  `orderno` INT NOT NULL,
+  `score` INT NULL,
+  `ts` DATETIME NOT NULL,
+  PRIMARY KEY (`giftid`, `ts`, `userno`, `viewerid`));
