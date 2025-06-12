d1f261a1519b0817a6bc4d69668616a5fc174eea
“Chouette2100” <chouette.21.00@gmail.com>
2025-06-12 22:23:12 +0900

    qa_records...Mroongaで作成する accesslog_trigger...botを追加する

--- 20250214_accesslog_trigger.sql
+++ 20250214_accesslog_trigger.sql
@@ -1,4 +1,4 @@
-ALTER TABLE accesslog ADD COLUMN is_bot TINYINT(1) DEFAULT 0;
+-- ALTER TABLE accesslog ADD COLUMN is_bot TINYINT(1) DEFAULT 0;
 
 DROP TRIGGER IF EXISTS set_is_bot;
 
@@ -7,7 +7,7 @@ DELIMITER //
 CREATE TRIGGER set_is_bot BEFORE INSERT ON accesslog
 FOR EACH ROW
 BEGIN
-    IF NEW.useragent REGEXP 'SemrushBot|Googlebot|Bytespider|bingbot|GPTBot|Amazonbot|Facebot|Twitterbot|applebot|trendictionbot|MJ12bot|serpstatbot' THEN
+    IF NEW.useragent REGEXP 'SemrushBot|Googlebot|Bytespider|bingbot|GPTBot|ClaudeBot|Amazonbot|Facebot|Twitterbot|applebot|trendictionbot|MJ12bot|serpstatbot|AhrefsBot|meta-externalagent|TikTokSpider|OAI-SearchBot|SeznamBot|bot@linkfluence|ChatGPT-User|PerplexityBot|msnbot|Timpibot' THEN
         SET NEW.is_bot = 1;
     ELSE
         SET NEW.is_bot = 0;
@@ -17,12 +17,15 @@ END; //
 DELIMITER ;
 
 UPDATE accesslog SET is_bot = 1
-    WHERE useragent REGEXP 'SemrushBot|Googlebot|Bytespider|bingbot|GPTBot|Amazonbot|Facebot|Twitterbot|applebot|trendictionbot|MJ12bot|serpstatbot'
+    WHERE useragent REGEXP 'SemrushBot|Googlebot|Bytespider|bingbot|GPTBot|ClaudeBot|Amazonbot|Facebot|Twitterbot|applebot|trendictionbot|MJ12bot|serpstatbot|AhrefsBot|meta-externalagent|TikTokSpider|OAI-SearchBot|SeznamBot|bot@linkfluence|ChatGPT-User|PerplexityBot|msnbot|Timpibot'
       AND is_bot = 0;
 
-SELECT * FROM accesslog
-WHERE useragent NOT REGEXP 'SemrushBot|Googlebot|Bytespider|bingbot|GPTBot|Amazonbot|Facebot|Twitterbot|applebot|trendictionbot|MJ12bot|serpstatbot'
-ORDER BY ts DESC;
+SELECT count(*) FROM accesslog
+    WHERE useragent NOT REGEXP 'SemrushBot|Googlebot|Bytespider|bingbot|GPTBot|ClaudeBot|Amazonbot|Facebot|Twitterbot|applebot|trendictionbot|MJ12bot|serpstatbot|AhrefsBot|meta-externalagent|TikTokSpider|OAI-SearchBot|SeznamBot|bot@linkfluence|ChatGPT-User|PerplexityBot|msnbot|Timpibot'
+  AND is_bot = 0
+  AND ts > '2024-01-01 00:00:00';
+-- ORDER BY ts DESC;
 
-SELECT * FROM accesslog
-WHERE useragent REGEXP 'Bot|bot|BOT' and is_bot = 0 order by useragent;
+SELECT count(*) FROM accesslog
+-- WHERE useragent REGEXP 'Bot|bot|BOT|Spider|spider' and is_bot = 0 order by useragent;
+WHERE useragent REGEXP 'Bot|bot|BOT|Spider|spider' and is_bot = 0;
--- 20250308_qa_records.sql
+++ 20250308_qa_records.sql
@@ -1,3 +1,4 @@
+/*
 CREATE TABLE qa_records (
 id INT AUTO_INCREMENT PRIMARY KEY,
 timestamp DATETIME NOT NULL,
@@ -8,3 +9,32 @@ maxtokens INT NOT NULL,
 temperature FLOAT NOT NULL,
 answer TEXT NOT NULL
 );
+
+ALTER TABLE `showroom`.`qa_records` 
+ADD COLUMN `system` VARCHAR(2048) NOT NULL AFTER `temperature`,
+ADD COLUMN `itokens` INT NOT NULL AFTER `answer`,
+ADD COLUMN `otokens` INT NOT NULL AFTER `itokens`,
+ADD COLUMN `stopreason` VARCHAR(256) NOT NULL AFTER `otokens`,
+CHANGE COLUMN `question` `question` TEXT NOT NULL AFTER `system`,
+CHANGE COLUMN `modelname` `modelname` VARCHAR(255) NOT NULL ;
+
+ALTER TABLE showroom.qa_records ADD FULLTEXT(question, answer);
+*/
+
+
+CREATE TABLE `qa_records` (
+     	`id` int NOT NULL AUTO_INCREMENT,
+     	`timestamp` datetime NOT NULL,
+     	`responsetime` int NOT NULL,
+     	`modelname` varchar(255) NOT NULL,
+     	`maxtokens` int NOT NULL,
+     	`temperature` float NOT NULL,
+     	`system` varchar(2048) NOT NULL,
+     	`question` text NOT NULL,
+	`answer` text NOT NULL,
+      	`itokens` int NOT NULL,
+      	`otokens` int NOT NULL,
+      	`stopreason` varchar(256) NOT NULL,
+	PRIMARY KEY (`id`),
+	FULLTEXT INDEX ft_question_answer(question, answer)
+) ENGINE=Mroonga DEFAULT CHARSET=utf8mb4;
